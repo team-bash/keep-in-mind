d20185bfb3e49c43da7d614a9ec7a0a1b7ec08e8
Nilson Nieto López <nilson-sn@outlook.com>
2020-04-26 19:25:27 -0500

    [update] remote unnecessary imports and format

--- src/main/java/com/teambash/keepinmind/service/SynonymService.java
+++ src/main/java/com/teambash/keepinmind/service/SynonymService.java
@@ -1,19 +1,14 @@
 package com.teambash.keepinmind.service;
 
 import com.teambash.keepinmind.shared.dto.SynonymDto;
-import com.teambash.keepinmind.shared.dto.WordDto;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface SynonymService {
 
-    SynonymDto createSynonym(SynonymDto dto);
+  SynonymDto createSynonym(SynonymDto dto);
 
-    List<SynonymDto> findAllSynonyms();
+  List<SynonymDto> findAllSynonyms();
 
-    SynonymDto getSynonym(Long id);
-
-
-
+  SynonymDto getSynonym(Long id);
 }
